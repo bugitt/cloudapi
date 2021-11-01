04373a685fb3af473bf04798ab90e3e9bb7717ef
loheagn <loheagn@icloud.com>
2021-11-01 10:30:27 +0800

    Initial commit

---
+++ src/main/kotlin/cn/edu/buaa/scs/cloudapi/CloudapiApplication.kt
@@ -0,0 +1,11 @@
+package cn.edu.buaa.scs.cloudapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloudapiApplication>(*args)
+}
---
+++ src/test/kotlin/cn/edu/buaa/scs/cloudapi/CloudapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.edu.buaa.scs.cloudapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
