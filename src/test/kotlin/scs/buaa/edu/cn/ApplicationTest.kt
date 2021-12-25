b304113b2aa8a3f2d0863cdb6f736b7369b3b817
loheagn <loheagn@icloud.com>
2021-12-25 17:38:00 +0800

    Refactor test module

--- src/test/kotlin/scs/buaa/edu/cn/ApplicationTest.kt
+++ src/test/kotlin/scs/buaa/edu/cn/ApplicationTest.kt
@@ -1,18 +1,1 @@
 package scs.buaa.edu.cn
-
-import cn.edu.buaa.scs.kubeClient
-import cn.edu.buaa.scs.kubeModule
-import io.ktor.application.*
-import io.ktor.server.testing.*
-import kotlin.test.Test
-
-class ApplicationTest {
-    @Test
-    fun testKubernetesInit() {
-        withTestApplication(Application::kubeModule) {
-            val client = kubeClient()
-            println(client.kubernetesVersion.gitVersion)
-            assert(client.kubernetesVersion.gitVersion.isNotBlank())
-        }
-    }
-}
