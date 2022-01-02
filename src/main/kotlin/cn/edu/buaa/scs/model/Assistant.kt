6cb2a86f63294ba5a24ee4fa13df01ee6ca7041f
loheagn <loheagn@icloud.com>
2022-01-02 22:43:00 +0800

    Add Assistant Model

---
+++ src/main/kotlin/cn/edu/buaa/scs/model/Assistant.kt
@@ -0,0 +1,36 @@
+package cn.edu.buaa.scs.model
+
+import org.ktorm.database.Database
+import org.ktorm.entity.Entity
+import org.ktorm.entity.sequenceOf
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+interface Assistant : Entity<Assistant> {
+    companion object : Entity.Factory<Assistant>()
+
+    var id: Int
+    var studentId: String
+    var courseId: String
+    var createTime: String
+}
+
+object Assistants : Table<Assistant>("assistant") {
+    @Suppress("unused")
+    val id = int("id").primaryKey().bindTo { it.id }
+
+    @Suppress("unused")
+    val studentId = varchar("student_id").bindTo { it.studentId }
+
+    @Suppress("unused")
+    val courseId = varchar("course_id").bindTo { it.courseId }
+
+    @Suppress("unused")
+    val createTime = varchar("create_time").bindTo { it.createTime }
+}
+
+@Suppress("unused")
+private val Database.assistants
+    get() = this.sequenceOf(Assistants)
+
