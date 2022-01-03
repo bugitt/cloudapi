0473cb91559a9f26fede17cc35fb0a48aaafef1e
loheagn <loheagn@icloud.com>
2022-01-03 18:49:41 +0800

    :white_check_mark: add s3Test

--- src/test/kotlin/cn/edu/buaa/scs/ApplicationTest.kt
+++ src/test/kotlin/cn/edu/buaa/scs/ApplicationTest.kt
@@ -1,1 +1,9 @@
 package cn.edu.buaa.scs
+
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.*
+import io.ktor.server.testing.*
+
+val testEnv = createTestEnvironment {
+    config = HoconApplicationConfig(ConfigFactory.load("application.conf"))
+}
