b4e0bf5290484e1aea82d3cb2d7a84ba7ed95eea
loheagn <loheagn@icloud.com>
2022-01-03 18:49:30 +0800

    :sparkles: add s3 module

--- src/main/kotlin/cn/edu/buaa/scs/storage/S3.kt
+++ src/main/kotlin/cn/edu/buaa/scs/storage/S3.kt
@@ -1,2 +1,29 @@
 package cn.edu.buaa.scs.storage
 
+import cn.edu.buaa.scs.getConfigString
+import cn.edu.buaa.scs.utils.logger
+import io.ktor.application.*
+import io.minio.BucketExistsArgs
+import io.minio.MinioClient
+
+lateinit var minioClient: MinioClient
+
+
+@Suppress("unused")
+fun Application.s3Module() {
+    val endPoint = getConfigString("s3.endPoint")
+    val accessKey = getConfigString("s3.accessKey")
+    val secretKey = getConfigString("s3.secretKey")
+    minioClient = MinioClient.builder()
+        .endpoint(endPoint)
+        .credentials(accessKey, secretKey)
+        .build()
+
+    logger("s3")().info {
+        "connected to s3-server successfully: ${
+            minioClient.bucketExists(
+                BucketExistsArgs.builder().bucket("scsos").build()
+            )
+        }"
+    }
+}
