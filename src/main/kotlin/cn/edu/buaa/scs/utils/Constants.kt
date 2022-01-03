37051636f3fef44617bfc5089acca222a4ff1b71
loheagn <loheagn@icloud.com>
2022-01-03 11:27:10 +0800

    :recycle: :sparkles: add super tokens

--- src/main/kotlin/cn/edu/buaa/scs/utils/Constants.kt
+++ src/main/kotlin/cn/edu/buaa/scs/utils/Constants.kt
@@ -8,3 +8,6 @@ val TOKEN_KEY = AttributeKey<String>(TOKEN)
 
 const val USER_KEY_NAME = "user"
 val USER_KEY = AttributeKey<User>(USER_KEY_NAME)
+
+const val USER_ID_KEY_NAME = "user_id"
+val USER_ID_KEY = AttributeKey<String>(USER_ID_KEY_NAME)
