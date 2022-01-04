9fd3cb3c009bb2d7a8acca21195f53d164be16b0
loheagn <loheagn@icloud.com>
2022-01-04 09:41:42 +0800

    :sparkles: 添加S3相关的文件管理功能

--- src/main/kotlin/cn/edu/buaa/scs/storage/S3.kt
+++ src/main/kotlin/cn/edu/buaa/scs/storage/S3.kt
@@ -2,11 +2,70 @@ package cn.edu.buaa.scs.storage
 
 import cn.edu.buaa.scs.getConfigString
 import cn.edu.buaa.scs.utils.logger
+import cn.edu.buaa.scs.utils.value
 import io.ktor.application.*
-import io.minio.BucketExistsArgs
-import io.minio.MinioClient
+import io.ktor.http.*
+import io.ktor.http.ContentType.Application.OctetStream
+import io.minio.*
+import java.io.InputStream
+
 
 lateinit var minioClient: MinioClient
+var minioPartSize: Long = 10485760L
+
+fun uploadFile(
+    bucket: String,
+    filename: String,
+    inputStream: InputStream,
+    contentType: ContentType = OctetStream
+) {
+    ensureBucketExists(bucket)
+    PutObjectArgs
+        .builder()
+        .bucket(bucket)
+        .`object`(filename)
+        .stream(inputStream, -1, minioPartSize)
+        .contentType(contentType.value)
+        .build()
+        .let { minioClient.putObject(it) }
+
+}
+
+fun deleteFile(bucket: String, filename: String) {
+    RemoveObjectArgs
+        .builder()
+        .bucket(bucket)
+        .`object`(filename)
+        .build()
+        .let { minioClient.removeObject(it) }
+}
+
+fun getFile(bucket: String, filename: String): InputStream =
+    GetObjectArgs
+        .builder()
+        .bucket(bucket)
+        .`object`(filename)
+        .build()
+        .let { minioClient.getObject(it) }
+
+fun downloadFile(bucket: String, filename: String, targetFileName: String) {
+    DownloadObjectArgs
+        .builder()
+        .bucket(bucket)
+        .`object`(filename)
+        .filename(targetFileName)
+        .build()
+        .let { minioClient.downloadObject(it) }
+}
+
+
+fun ensureBucketExists(bucket: String) {
+    minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build()).let {
+        if (!it) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build())
+        }
+    }
+}
 
 
 @Suppress("unused")
@@ -14,6 +73,10 @@ fun Application.s3Module() {
     val endPoint = getConfigString("s3.endPoint")
     val accessKey = getConfigString("s3.accessKey")
     val secretKey = getConfigString("s3.secretKey")
+    println(endPoint)
+    println(accessKey)
+    println(secretKey)
+    minioPartSize = getConfigString("s3.partSize").toLong()
     minioClient = MinioClient.builder()
         .endpoint(endPoint)
         .credentials(accessKey, secretKey)
--- src/test/kotlin/cn/edu/buaa/scs/storage/S3Test.kt
+++ src/test/kotlin/cn/edu/buaa/scs/storage/S3Test.kt
@@ -4,6 +4,11 @@ import cn.edu.buaa.scs.testEnv
 import io.ktor.server.testing.*
 import io.minio.BucketExistsArgs
 import org.junit.Test
+import java.io.File
+import java.io.FileInputStream
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
 
 class S3TEst {
     @Test
@@ -16,4 +21,50 @@ class S3TEst {
             )
         }
     }
+
+    @Test
+    fun testEnsureBucketExists() {
+        withApplication(testEnv) {
+            ensureBucketExists("scsos3")
+            assert(
+                minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket("scsos").build()
+                )
+            )
+        }
+    }
+
+    @Test
+    fun testFileManagement() {
+        withApplication(testEnv) {
+            val content = "12345"
+            val bucket = "tmp"
+            val filename = UUID.randomUUID().toString()
+            val fullFilePath = "${getBaseFilePath()}/$filename"
+
+            // upload
+            fullFilePath
+                .let { File(it).apply { writeText(content) } }
+                .let { FileInputStream(it) }
+                .use { uploadFile(bucket, filename, it) }
+
+            // get
+            assert(String(getFile(bucket, filename).readAllBytes()) == content)
+
+            // download
+            val newFilepath = "${getBaseFilePath()}/$filename-2"
+            downloadFile(bucket, filename, newFilepath)
+            assert(Files.exists(Paths.get(newFilepath)))
+
+            // delete
+            deleteFile(bucket, filename)
+
+            // delete local file
+            Files.delete(Paths.get(fullFilePath))
+            Files.delete(Paths.get(newFilepath))
+        }
+
+    }
+
+    private fun getBaseFilePath() = Paths.get(".").toAbsolutePath().toString()
 }
