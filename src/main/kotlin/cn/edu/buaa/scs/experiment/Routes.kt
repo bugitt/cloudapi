0e7ec0fde56f1699b5cf5603b295b4f3c4211c4d
loheagn <loheagn@icloud.com>
2022-01-07 23:27:53 +0800

    :sparkles: 完成 "重复上传作业" 接口

--- src/main/kotlin/cn/edu/buaa/scs/experiment/Routes.kt
+++ src/main/kotlin/cn/edu/buaa/scs/experiment/Routes.kt
@@ -10,36 +10,87 @@ import io.ktor.http.content.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.io.InputStream
 
 fun Route.experimentRoute() {
     route("/experiment/{experimentId}") {
+
+        fun ApplicationCall.getExpIdFromPath(): Int =
+            parameters["experimentId"]?.toInt()
+                ?: throw BadRequestException("experiment id is invalid")
+
         route("/assignment") {
+
+            suspend fun ApplicationCall.parseUserAndFile(): Triple<String, String, InputStream> {
+                val experimentId = getExpIdFromPath()
+                return receiveMultipart().readAllParts().let { partDataList ->
+                    val userId = getFormItem<PartData.FormItem>(partDataList, "userId")?.value ?: userId()
+
+                    // check permission
+                    assertPermission(userId == userId()) {
+                        assertExperiment(userId(), experimentId)
+                    }
+
+                    val fileItem = getFormItem<PartData.FileItem>(partDataList, "file")
+                        ?: throw BadRequestException("can not parse file item")
+                    Triple(userId, fileItem.originalFileName ?: "", fileItem.streamProvider())
+                }
+            }
 
             /**
              * 第一次上传作业
              */
             post {
-                val experimentId = call.parameters["experimentId"]?.toInt()
-                    ?: throw BadRequestException("experiment id is invalid")
-                call.receiveMultipart().readAllParts().let { partDataList ->
-                    val userId = getFormItem<PartData.FormItem>(partDataList, "userId")?.value ?: call.userId()
+                val experimentId = call.getExpIdFromPath()
+                call.parseUserAndFile()
+                    .let { (userId, filename, inputStream) ->
 
-                    // check permission
-                    call.assertPermission(userId == call.userId()) {
-                        assertExperiment(call.userId(), experimentId)
+                        // check permission
+                        call.assertPermission(userId == call.userId()) {
+                            assertExperiment(call.userId(), experimentId)
+                        }
+
+                        createAssignment(
+                            experimentId,
+                            userId,
+                            call.userId(),
+                            filename,
+                            inputStream
+                        )
+
                     }
+                    .let { call.respond(it) }
+            }
 
-                    val fileItem = getFormItem<PartData.FileItem>(partDataList, "file")
-                        ?: throw BadRequestException("can not parse file item")
-                    createAssignment(
-                        experimentId,
-                        userId,
-                        call.userId(),
-                        fileItem.originalFileName ?: "",
-                        fileItem.streamProvider()
-                    )
-                }.let {
-                    call.respond(it)
+            route("/{assignmentId}") {
+
+                fun ApplicationCall.getAssignmentIdFromPath(): Int =
+                    parameters["assignmentId"]?.toInt()
+                        ?: throw BadRequestException("assignmentId id is invalid")
+
+                /**
+                 * 重复提交作业
+                 */
+                put {
+                    val experimentId = call.getExpIdFromPath()
+                    val assignmentId = call.getAssignmentIdFromPath()
+                    call.parseUserAndFile()
+                        .let { (userId, filename, inputStream) ->
+
+                            // check permission
+                            call.assertPermission(userId == call.userId()) {
+                                assertExperiment(call.userId(), experimentId)
+                            }
+
+                            updateAssigmentFile(
+                                assignmentId,
+                                call.userId(),
+                                filename,
+                                inputStream
+                            )
+
+                        }
+                        .let { call.respond(it) }
                 }
             }
         }
