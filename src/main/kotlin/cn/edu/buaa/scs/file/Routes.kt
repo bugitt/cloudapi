929fddd2cfbf0ed0f27c4f4628aa669f49bfd176
loheagn <loheagn@icloud.com>
2022-01-28 19:42:37 +0800

    :sparkles: 添加修改文件的接口

--- src/main/kotlin/cn/edu/buaa/scs/file/Routes.kt
+++ src/main/kotlin/cn/edu/buaa/scs/file/Routes.kt
@@ -5,8 +5,11 @@ import cn.edu.buaa.scs.model.File
 import cn.edu.buaa.scs.model.files
 import cn.edu.buaa.scs.service.file
 import cn.edu.buaa.scs.storage.mysql
+import cn.edu.buaa.scs.utils.getFormItem
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.ktorm.dsl.eq
@@ -23,6 +26,16 @@ fun Route.fileRoute() {
             // TODO: 文件名
             // FIXME: 文件名
             call.respondOutputStream(status = HttpStatusCode.OK, producer = call.file.fetchProducer(file))
+        }
+
+        patch {
+            val fileItem = call.receiveMultipart().readAllParts().let { partDataList ->
+                getFormItem<PartData.FileItem>(partDataList, "file")
+                    ?: throw BadRequestException("can not parse file item")
+            }
+            call.file.update(call.getFile(), fileItem.streamProvider()).let {
+                call.respond(it)
+            }
         }
     }
 }
--- src/main/kotlin/cn/edu/buaa/scs/service/File.kt
+++ src/main/kotlin/cn/edu/buaa/scs/service/File.kt
@@ -8,6 +8,7 @@ import cn.edu.buaa.scs.model.StoreType
 import cn.edu.buaa.scs.storage.getFile
 import cn.edu.buaa.scs.storage.uploadFile
 import cn.edu.buaa.scs.utils.user
+import cn.edu.buaa.scs.utils.userId
 import io.ktor.application.*
 import java.io.InputStream
 import java.io.OutputStream
@@ -22,27 +23,57 @@ class FileService(val call: ApplicationCall) {
         val inputStream = getFile(file.storePath, file.storeName)
         return { inputStream.copyTo(this) }
     }
+
+    fun create(
+        owner: String,
+        originalName: String,
+        fileType: FileType,
+        inputStream: InputStream
+    ): File {
+        val fileResp = File.upload(fileType, inputStream)
+        return File {
+            name = originalName
+            storeType = StoreType.S3
+            storeName = fileResp.storeName
+            storePath = Assignment.bucket
+            uploadTime = fileResp.uploadTime
+            this.fileType = fileType
+            size = fileResp.size
+            this.uploader = uploader
+            this.owner = owner
+            this.createdAt = System.currentTimeMillis()
+            this.updatedAt = System.currentTimeMillis()
+        }
+    }
+
+    fun update(file: File, inputStream: InputStream): File {
+        File.upload(file.fileType, inputStream).let {
+            file.storeName = it.storeName
+            file.uploadTime = it.uploadTime
+            file.size = it.size
+            file.uploader = call.userId()
+            file.updatedAt = System.currentTimeMillis()
+
+            file.flushChanges()
+        }
+        return file
+    }
 }
 
 fun File.Companion.upload(
-    owner: String,
-    uploader: String,
-    originalName: String,
+    fileType: FileType,
     inputStream: InputStream
-): File {
+): FileResp {
     val storeFileName = UUID.randomUUID().toString()
-    val fileResp = uploadFile(Assignment.bucket, storeFileName, inputStream)
-    return File {
-        name = originalName
-        storeType = StoreType.S3
-        storeName = storeFileName
-        storePath = Assignment.bucket
-        uploadTime = fileResp.lastModified().toInstant().toEpochMilli()
-        fileType = FileType.Assignment
-        size = fileResp.size()
-        this.uploader = uploader
-        this.owner = owner
-        this.createdAt = System.currentTimeMillis()
-        this.updatedAt = System.currentTimeMillis()
+    return when (fileType) {
+        FileType.Assignment -> uploadFile(Assignment.bucket, storeFileName, inputStream)
+    }.let {
+        FileResp(storeFileName, it.size(), it.lastModified().toInstant().toEpochMilli())
     }
 }
+
+data class FileResp(
+    val storeName: String,
+    val size: Long,
+    val uploadTime: Long,
+)
