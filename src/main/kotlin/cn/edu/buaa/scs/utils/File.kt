77c7e6d164da3976772edca7cde3866efb824e19
loheagn <loheagn@icloud.com>
2022-02-01 21:14:43 +0800

    :bug: 修改文件时，保证能够更新

--- src/main/kotlin/cn/edu/buaa/scs/utils/File.kt
+++ src/main/kotlin/cn/edu/buaa/scs/utils/File.kt
@@ -5,3 +5,14 @@ fun String.getFileExtension(): String =
         if (it.size > 1) it.last()
         else ""
     }
+
+fun String.updateFileExtension(newFilename: String?): String {
+    val newExt = newFilename?.getFileExtension() ?: return this
+    return if (newExt.isNotBlank()) {
+        this.split(".").toMutableList().also { seq ->
+            seq[seq.size - 1] = newExt
+        }.joinToString(".")
+    } else {
+        this
+    }
+}
