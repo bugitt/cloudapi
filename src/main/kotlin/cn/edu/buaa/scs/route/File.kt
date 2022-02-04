f7f60758d9201a6ea5ab2a1b5b1a6441322c91a6
loheagn <loheagn@icloud.com>
2022-02-04 18:26:41 +0800

    :boom: 调整文件上传下载相关接口

--- src/main/kotlin/cn/edu/buaa/scs/route/File.kt
+++ src/main/kotlin/cn/edu/buaa/scs/route/File.kt
@@ -3,46 +3,25 @@ package cn.edu.buaa.scs.route
 import cn.edu.buaa.scs.controller.models.FileResponse
 import cn.edu.buaa.scs.error.BadRequestException
 import cn.edu.buaa.scs.model.File
-import cn.edu.buaa.scs.model.FileType
-import cn.edu.buaa.scs.model.files
 import cn.edu.buaa.scs.service.file
-import cn.edu.buaa.scs.storage.mysql
-import cn.edu.buaa.scs.utils.*
+import cn.edu.buaa.scs.service.id
+import cn.edu.buaa.scs.utils.BASE_URL
 import io.ktor.application.*
 import io.ktor.http.*
-import io.ktor.http.content.*
-import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.ktorm.dsl.eq
-import org.ktorm.entity.find
 import java.net.URI
 import java.net.URLEncoder
 
 fun Route.fileRoute() {
     route("/file") {
         post {
-            val readChannel = call.receiveChannel()
-            val headers = call.request.headers
-            call.file.createOrUpdate(
-                readChannel,
-                call.headerString("file-name"),
-                call.headerString("file-owner"),
-                try {
-                    FileType.valueOf(call.headerString("file-type"))
-                } catch (e: Exception) {
-                    throw BadRequestException("invalid file-type, think about ${FileType.values()}")
-                },
-                call.headerInt("involved-id"),
-                call.headerString("content-type"),
-                call.headerIntOrNull("file-id")
-            )
+            call.file.createOrUpdate().let {
+                call.respond(it)
+            }
         }
 
         route("/{fileId}") {
-            fun ApplicationCall.getFile(): File =
-                parameters["fileId"]?.toInt()?.let { fileId -> mysql.files.find { it.id eq fileId } }
-                    ?: throw BadRequestException("file id is invalid")
 
             fun ApplicationCall.getFileIdFromPath(): Int =
                 parameters["fileId"]?.toInt()
@@ -54,35 +33,26 @@ fun Route.fileRoute() {
                 }
             }
 
-            patch {
-                val fileItem = call.receiveMultipart().readAllParts().let { partDataList ->
-                    getFormItem<PartData.FileItem>(partDataList, "file")
-                        ?: throw BadRequestException("can not parse file item")
-                }
-                call.file.update(call.getFile(), fileItem.originalFileName, fileItem.streamProvider()).let {
-                    call.respond(it)
-                }
-            }
-
             route("/content") {
+
+                // 下载文件
                 get {
-                    val file = call.getFile()
-                    call.response.header(
-                        HttpHeaders.ContentDisposition,
-                        ContentDisposition.Attachment.withParameter(
-                            ContentDisposition.Parameters.FileName,
-                            URLEncoder.encode(file.name, "utf-8")
-                        ).toString()
-                    )
-                    call.response.header(
-                        "file-size",
-                        file.size
-                    )
-                    call.respondOutputStream(
-                        contentType = ContentType.parse(file.contentType),
-                        status = HttpStatusCode.OK,
-                        producer = call.file.fetchProducer(file)
-                    )
+                    val file = File.id(call.getFileIdFromPath())
+                    call.file.downloadFile(file).let {
+                        call.response.header(
+                            HttpHeaders.ContentDisposition,
+                            ContentDisposition.Attachment.withParameter(
+                                ContentDisposition.Parameters.FileName,
+                                @Suppress("BlockingMethodInNonBlockingContext")
+                                URLEncoder.encode(file.name, "utf-8")
+                            ).toString()
+                        )
+                        call.response.header(
+                            "fixed-content-type",
+                            file.contentType
+                        )
+                        call.respondFile(it)
+                    }
                 }
             }
         }
--- src/main/kotlin/cn/edu/buaa/scs/service/Assignment.kt
+++ src/main/kotlin/cn/edu/buaa/scs/service/Assignment.kt
@@ -1,6 +1,5 @@
 package cn.edu.buaa.scs.service
 
-import cn.edu.buaa.scs.auth.assertAdmin
 import cn.edu.buaa.scs.auth.assertRead
 import cn.edu.buaa.scs.auth.assertWrite
 import cn.edu.buaa.scs.auth.authAdmin
@@ -8,6 +7,7 @@ import cn.edu.buaa.scs.error.AuthorizationException
 import cn.edu.buaa.scs.error.BusinessException
 import cn.edu.buaa.scs.error.NotFoundException
 import cn.edu.buaa.scs.model.*
+import cn.edu.buaa.scs.storage.S3
 import cn.edu.buaa.scs.storage.mysql
 import cn.edu.buaa.scs.utils.exists
 import cn.edu.buaa.scs.utils.getFileExtension
@@ -18,12 +18,15 @@ import org.ktorm.dsl.eq
 import org.ktorm.entity.add
 import org.ktorm.entity.find
 import org.ktorm.entity.update
-import java.io.InputStream
 
 val ApplicationCall.assignment: AssignmentService
     get() = AssignmentService(this)
 
-class AssignmentService(val call: ApplicationCall) {
+class AssignmentService(val call: ApplicationCall) : FileService.IFileUploadService {
+
+    companion object {
+        private const val bucketName = "scs-assignment"
+    }
 
     fun create(expId: Int, owner: String): Assignment {
         if (mysql.assignments.exists { (it.studentId eq owner) and (it.expId eq expId) }) {
@@ -62,68 +65,22 @@ class AssignmentService(val call: ApplicationCall) {
         return assignment
     }
 
-    fun create(
-        expId: Int,
-        owner: String,
-        originalName: String,
-        inputStream: InputStream
-    ): Assignment {
-        // 检查是或否已经提交过作业了
-        if (mysql.assignments.exists { (it.studentId eq owner) and (it.expId eq expId) }) {
-            throw BusinessException("student($owner) has already created assignment for exp($expId)")
-        }
-
-        val assignment = Assignment {
-            studentId = owner
-            this.expId = expId
-            this.courseId = Experiment.id(expId).courseId
-            this.createdAt = System.currentTimeMillis()
-            this.updatedAt = System.currentTimeMillis()
-        }
-
-        call.user().assertAdmin(assignment)
-
-        val file = call.file.create(owner, buildFilename(expId, owner, originalName), FileType.Assignment, inputStream)
-        mysql.useTransaction {
-            // create assignment
-            mysql.assignments.add(assignment)
-            updateAssignmentFile(assignment, file)
-        }
-        return assignment
+    override fun uploader(): S3 {
+        return S3(bucketName)
     }
 
-    fun update(
-        assignment: Assignment,
-        originalName: String,
-        inputStream: InputStream
-    ): Assignment {
-        call.user().assertWrite(assignment)
-        val file = call.file.create(
-            assignment.studentId,
-            buildFilename(assignment.expId, assignment.studentId, originalName),
-            FileType.Assignment,
-            inputStream
-        )
-        mysql.useTransaction {
-            updateAssignmentFile(assignment, file)
-        }
-        return assignment
-    }
-
-    private fun buildFilename(expId: Int, ownerId: String, originalName: String): String {
+    override fun fixName(originalName: String?, ownerId: String, involvedId: Int): String {
         val owner = User.id(ownerId)
-        return "${owner.name}_${owner.id}_${Experiment.id(expId).name}.${originalName.getFileExtension()}"
+        val assignment = Assignment.id(involvedId)
+        return "${owner.name}_${owner.id}_${Experiment.id(assignment.expId).name}.${(originalName ?: "").getFileExtension()}"
     }
 
-    private fun updateAssignmentFile(
-        assignment: Assignment,
-        file: File
-    ) {
-        file.involvedId = assignment.id
-        mysql.files.add(file)
-        assignment.fileId = file.id
-        assignment.updatedAt = System.currentTimeMillis()
-        mysql.assignments.update(assignment)
+    override fun checkOwner(ownerId: String, involvedId: Int): Boolean {
+        return Assignment.id(involvedId).studentId == ownerId
+    }
+
+    override fun storePath(): String {
+        return bucketName
     }
 }
 
@@ -131,9 +88,6 @@ fun Assignment.Companion.id(id: Int): Assignment {
     return mysql.assignments.find { it.id eq id }
         ?: throw BusinessException("find course($id) from database error")
 }
-
-val Assignment.Companion.bucket: String
-    get() = "scs-assignment"
 
 fun Assignment.getCourse(): Course = Course.id(this.courseId)
 
