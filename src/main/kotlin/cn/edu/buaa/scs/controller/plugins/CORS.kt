591bdefb0343f25769cd2b7274dd7b5f4b3dc7be
loheagn <loheagn@icloud.com>
2022-02-09 17:21:44 +0800

    :bug: allow cros

--- src/main/kotlin/cn/edu/buaa/scs/controller/plugins/CORS.kt
+++ src/main/kotlin/cn/edu/buaa/scs/controller/plugins/CORS.kt
@@ -2,9 +2,23 @@ package cn.edu.buaa.scs.controller.plugins
 
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.*
 
 fun Application.configureCORS() {
     install(CORS) {
         anyHost()
+
+        method(HttpMethod.Options)
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
+        method(HttpMethod.Put)
+        method(HttpMethod.Patch)
+        method(HttpMethod.Delete)
+
+        header(HttpHeaders.ContentType)
+        header(HttpHeaders.Authorization)
+        allowHeadersPrefixed("x-scs-")
+
+        maxAgeInSeconds = 3600
     }
 }
