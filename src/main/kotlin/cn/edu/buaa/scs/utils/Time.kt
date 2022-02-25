e5c12cd5d342eed64d157a052b5ca4fafbbd6ed6
loheagn <loheagn@icloud.com>
2022-02-25 14:24:42 +0800

    :bug: 不允许在实验结束之后提交作业

--- src/main/kotlin/cn/edu/buaa/scs/utils/Time.kt
+++ src/main/kotlin/cn/edu/buaa/scs/utils/Time.kt
@@ -4,9 +4,18 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
+const val defaultDateTimeFormatter = "yyyy/MM/dd HH:mm:ss"
+
+fun String.toTimestamp(formatString: String = defaultDateTimeFormatter): Long {
+    val zonedDateTime =
+        LocalDateTime.parse(this, DateTimeFormatter.ofPattern(formatString)).atZone(ZoneId.of("Asia/Shanghai"))
+    return zonedDateTime.toInstant().toEpochMilli()
+}
 
 class InstantSerializer : StdSerializer<Instant>(Instant::class.java) {
     override fun serialize(value: Instant?, gen: JsonGenerator?, provider: SerializerProvider?) {
