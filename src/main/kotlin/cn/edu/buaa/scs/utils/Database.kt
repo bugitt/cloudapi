0c4d268d292f7ae284e24c049f8f84ad307098e6
loheagn <loheagn@icloud.com>
2022-02-26 17:46:33 +0800

    :zap: 优化检查数据库是否已存在满足条件的记录

--- src/main/kotlin/cn/edu/buaa/scs/utils/Database.kt
+++ src/main/kotlin/cn/edu/buaa/scs/utils/Database.kt
@@ -1,9 +1,9 @@
 package cn.edu.buaa.scs.utils
 
 import org.ktorm.entity.EntitySequence
-import org.ktorm.entity.find
+import org.ktorm.entity.count
 import org.ktorm.schema.BaseTable
 import org.ktorm.schema.ColumnDeclaring
 
 fun <E : Any, T : BaseTable<E>> EntitySequence<E, T>.exists(predicate: (T) -> ColumnDeclaring<Boolean>): Boolean =
-    find(predicate) != null
+    count(predicate) != 0
