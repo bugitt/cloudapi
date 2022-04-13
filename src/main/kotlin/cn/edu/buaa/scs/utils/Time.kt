22ee85a371d8e551235f643d4bae3772f08b7067
loheagn <loheagn@icloud.com>
2022-04-13 14:29:20 +0800

    :construction: Try to connect the vCenter service

--- src/main/kotlin/cn/edu/buaa/scs/utils/Time.kt
+++ src/main/kotlin/cn/edu/buaa/scs/utils/Time.kt
@@ -1,12 +1,8 @@
 package cn.edu.buaa.scs.utils
 
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 const val defaultDateTimeFormatter = "yyyy/MM/dd HH:mm:ss"
@@ -27,12 +23,4 @@ fun Long.formatDateTime(formatString: String = defaultDateTimeFormatter): String
 object TimeUtil {
     fun currentDateTime(): String =
         System.currentTimeMillis().formatDateTime()
-}
-
-class InstantSerializer : StdSerializer<Instant>(Instant::class.java) {
-    override fun serialize(value: Instant?, gen: JsonGenerator?, provider: SerializerProvider?) {
-        val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
-            .withZone(ZoneOffset.ofHours(8))
-        gen?.writeString(value?.let { formatter.format(it) })
-    }
 }
