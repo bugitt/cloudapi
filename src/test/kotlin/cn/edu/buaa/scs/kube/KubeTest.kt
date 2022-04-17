54078de5fec856243fae1ada015e89f59c5cb8fb
loheagn <loheagn@icloud.com>
2022-04-17 10:40:44 +0800

    :white_check_mark: Improve unit test for kubernetes client `watch`
    Signed-off-by: loheagn <loheagn@icloud.com>

--- src/test/kotlin/cn/edu/buaa/scs/kube/KubeTest.kt
+++ src/test/kotlin/cn/edu/buaa/scs/kube/KubeTest.kt
@@ -1,6 +1,12 @@
 package cn.edu.buaa.scs.kube
 
-import io.fabric8.kubernetes.api.model.apps.Deployment
+import cn.edu.buaa.scs.utils.logger
+import com.fkorotkov.kubernetes.newObjectMeta
+import com.fkorotkov.kubernetes.newPersistentVolumeClaim
+import com.fkorotkov.kubernetes.resources
+import com.fkorotkov.kubernetes.spec
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaim
+import io.fabric8.kubernetes.api.model.Quantity
 import io.fabric8.kubernetes.client.Watcher
 import io.fabric8.kubernetes.client.WatcherException
 import io.ktor.application.*
@@ -19,24 +25,38 @@ class KubeTest {
     }
 
     @Test
-    fun testKubernetesEventWatch() {
+    fun testWatch() {
         withTestApplication(Application::kubeModule) {
-            kubeClient().use { client ->
-                client.batch().v1().jobs()
-                client.apps().deployments().inAnyNamespace().watch(object : Watcher<Deployment> {
-                    override fun eventReceived(action: Watcher.Action?, resource: Deployment?) {
-                        println("$action, $resource")
-                    }
+            val client = kubeClient()
 
-                    override fun onClose(cause: WatcherException?) {
-                        println(cause.toString())
-                    }
+            client.persistentVolumeClaims().watch(object : Watcher<PersistentVolumeClaim> {
+                override fun eventReceived(action: Watcher.Action?, resource: PersistentVolumeClaim?) {
+                    logger()().info("Watch event received {}: {}", action?.name, resource?.metadata?.name)
+                }
 
-                }).use {
-                    Thread.sleep(100000L)
+                override fun onClose(cause: WatcherException?) {
+                    logger()().info("Watch gracefully closed")
+                }
+
+            }).use {
+                val pvc = client.persistentVolumeClaims().create(
+                    newPersistentVolumeClaim {
+                        metadata = newObjectMeta {
+                            name = "test"
+                            namespace = "default"
+                        }
+                        spec {
+                            accessModes = listOf("ReadWriteMany")
+                            resources {
+                                requests = mapOf("storage" to Quantity("1Gi"))
+                            }
+                        }
+                    }
+                )
+                pvc?.let {
+                    client.persistentVolumeClaims().delete(it)
                 }
             }
-
         }
     }
 }
