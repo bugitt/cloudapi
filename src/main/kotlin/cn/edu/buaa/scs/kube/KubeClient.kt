8fc3ab9b717f4ca85aa815b15e4c94d914356f5f
loheagn <loheagn@icloud.com>
2022-06-20 16:03:20 +0800

    :poop: remove kubernetes module
    Signed-off-by: loheagn <loheagn@icloud.com>

--- src/main/kotlin/cn/edu/buaa/scs/kube/KubeClient.kt
+++ src/main/kotlin/cn/edu/buaa/scs/kube/KubeClient.kt
@@ -1,8 +1,5 @@
 package cn.edu.buaa.scs.kube
 
-import cn.edu.buaa.scs.utils.getFile
-import cn.edu.buaa.scs.utils.logger
-import io.fabric8.kubernetes.client.DefaultKubernetesClient
 import io.fabric8.kubernetes.client.KubernetesClient
 import io.ktor.application.*
 
@@ -10,12 +7,12 @@ lateinit var kubeClient: () -> KubernetesClient
 
 @Suppress("unused")
 fun Application.kubeModule() {
-    val configString = String(getFile("/kubeconfig.yaml").readAllBytes())
-    kubeClient = fun(): KubernetesClient {
-        return DefaultKubernetesClient.fromConfig(configString)
-    }
-    KubeOpScheduler.launch()
-    kubeClient().use {
-        logger("kube")().info { "connected to kubernetes-appserver successfully: ${it.kubernetesVersion.gitVersion}" }
-    }
+//    val configString = String(getFile("/kubeconfig.yaml").readAllBytes())
+//    kubeClient = fun(): KubernetesClient {
+//        return DefaultKubernetesClient.fromConfig(configString)
+//    }
+//    KubeOpScheduler.launch()
+//    kubeClient().use {
+//        logger("kube")().info { "connected to kubernetes-appserver successfully: ${it.kubernetesVersion.gitVersion}" }
+//    }
 }
