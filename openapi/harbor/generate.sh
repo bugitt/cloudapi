89e0ca54a1099309acc754d03891634ec23dcde1
loheagn <loheagn@icloud.com>
2022-08-22 11:32:40 +0800

    Rename the config file for the harbor api generator
    Signed-off-by: loheagn <loheagn@icloud.com>

--- openapi/harbor/generate.sh
+++ openapi/harbor/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openapi-generator generate -g kotlin -i ./harbor_api.yaml -c config.yaml -o ./tmp
+openapi-generator generate -g kotlin -i ./harbor_api.yaml -c openapi_generator_config.yaml -o ./tmp
 
 src_dir="./tmp/src/main/kotlin/cn/edu/buaa/scs/sdk/harbor/"
 des_dir="../../src/main/kotlin/cn/edu/buaa/scs/sdk/harbor"
