05fad7d2a2b3c7fa6828faed806b0a26d6aa7231
loheagn <loheagn@icloud.com>
2022-08-26 23:01:39 +0800

    :bug: Initial KubeClient
    Signed-off-by: loheagn <loheagn@icloud.com>

--- src/main/kotlin/cn/edu/buaa/scs/kube/KubeModule.kt
+++ src/main/kotlin/cn/edu/buaa/scs/kube/KubeModule.kt
@@ -2,17 +2,24 @@ package cn.edu.buaa.scs.kube
 
 import cn.edu.buaa.scs.utils.getConfigString
 import cn.edu.buaa.scs.utils.logger
-import io.fabric8.kubernetes.client.DefaultKubernetesClient
+import io.fabric8.kubernetes.client.ConfigBuilder
 import io.fabric8.kubernetes.client.KubernetesClient
+import io.fabric8.kubernetes.client.KubernetesClientBuilder
 import io.ktor.server.application.*
 
 lateinit var kubeClient: () -> KubernetesClient
 
 @Suppress("unused")
 fun Application.kubeModule() {
-    val configString = getConfigString("kube.business.configYaml")
+    val masterUrl = getConfigString("kube.business.masterUrl")
+    val token = getConfigString("kube.business.token")
     kubeClient = fun(): KubernetesClient {
-        return DefaultKubernetesClient.fromConfig(configString)
+        val config = ConfigBuilder()
+            .withMasterUrl(masterUrl)
+            .withApiVersion("v1")
+            .withOauthToken(token)
+            .build()
+        return KubernetesClientBuilder().withConfig(config).build()
     }
     KubeOpScheduler.launch()
     kubeClient().use {
