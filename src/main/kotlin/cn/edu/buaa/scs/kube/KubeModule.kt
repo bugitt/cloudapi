ece715fa3999918543e15cd550ae5fce93314b81
loheagn <loheagn@icloud.com>
2022-08-26 21:55:20 +0800

    :sparkles: Add kubeModule
    Signed-off-by: loheagn <loheagn@icloud.com>

---
+++ src/main/kotlin/cn/edu/buaa/scs/kube/KubeModule.kt
@@ -0,0 +1,21 @@
+package cn.edu.buaa.scs.kube
+
+import cn.edu.buaa.scs.utils.getConfigString
+import cn.edu.buaa.scs.utils.logger
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+import io.fabric8.kubernetes.client.KubernetesClient
+import io.ktor.server.application.*
+
+lateinit var kubeClient: () -> KubernetesClient
+
+@Suppress("unused")
+fun Application.kubeModule() {
+    val configString = getConfigString("kube.business.configYaml")
+    kubeClient = fun(): KubernetesClient {
+        return DefaultKubernetesClient.fromConfig(configString)
+    }
+    KubeOpScheduler.launch()
+    kubeClient().use {
+        logger("kube")().info { "connected to business-kubernetes-appserver successfully: ${it.kubernetesVersion.gitVersion}" }
+    }
+}
