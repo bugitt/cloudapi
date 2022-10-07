8e73d8a5969361a1ebdb46e345cf0ca8d509b2e5
loheagn <loheagn@icloud.com>
2022-10-07 15:35:42 +0800

    Extend the lifetime of token on each call
    Signed-off-by: loheagn <loheagn@icloud.com>

--- src/main/kotlin/cn/edu/buaa/scs/utils/Redis.kt
+++ src/main/kotlin/cn/edu/buaa/scs/utils/Redis.kt
@@ -1,11 +1,14 @@
 package cn.edu.buaa.scs.utils
 
 import io.lettuce.core.RedisClient
+import io.lettuce.core.SetArgs
 
 /**
  * 连接 authRedis， 以判断token是否有效
  */
 fun RedisClient.checkToken(token: String): String? =
     this.connect().use {
-        it.async().get(token)?.get()?.trim('"')?.lowercase()
+        val value = it.async().get(token)?.get()
+        it.async().set(token, value, SetArgs.Builder.ex(60 * 60 * 24))
+        value?.trim('"')?.lowercase()
     }
