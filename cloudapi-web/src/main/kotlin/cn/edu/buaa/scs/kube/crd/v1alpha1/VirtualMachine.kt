3856baf9fb88eaa876e77f24b559ebbec3dd673a
HanZiyao <h56983577@126.com>
2025-01-05 15:55:17 +0800

    若出现异常，捕获后直接在函数中重试

--- cloudapi-web/src/main/kotlin/cn/edu/buaa/scs/kube/crd/v1alpha1/VirtualMachine.kt
+++ cloudapi-web/src/main/kotlin/cn/edu/buaa/scs/kube/crd/v1alpha1/VirtualMachine.kt
@@ -129,111 +129,115 @@ class VirtualMachineReconciler(val client: KubernetesClient) : Reconciler<Virtua
         resource: VirtualMachine?,
         context: Context<VirtualMachine>?
     ): UpdateControl<VirtualMachine> {
-        try {
-            val vm = resource ?: return UpdateControl.noUpdate()
-            val vmClient = newVMClient(vm.spec.platform)
-            if (vm.spec.deleted) {
-                val exist = runBlocking {
-                    try {
-                        if (vmClient.getVM(vm.status.uuid).isSuccess) {
-                            vmClient.deleteVM(vm.status.uuid).getOrThrow()
-                            true
-                        } else {
+        // 出现异常重试 10 次
+        for (i in 1..10) {
+            try {
+                val vm = resource ?: return UpdateControl.noUpdate()
+                val vmClient = newVMClient(vm.spec.platform)
+                if (vm.spec.deleted) {
+                    val exist = runBlocking {
+                        try {
+                            if (vmClient.getVM(vm.status.uuid).isSuccess) {
+                                vmClient.deleteVM(vm.status.uuid).getOrThrow()
+                                true
+                            } else {
+                                false
+                            }
+                        } catch (e: NullPointerException) {
                             false
                         }
-                    } catch (e: NullPointerException) {
-                        false
+                    }
+                    return if (exist) {
+                        UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
+                    } else {
+                        UpdateControl.noUpdate()
                     }
                 }
-                return if (exist) {
-                    UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
-                } else {
-                    UpdateControl.noUpdate()
-                }
-            }
 
-            logger("vm-reconcile")().info { "Reconciling VirtualMachine: ${vm.spec.name}" }
+                logger("vm-reconcile")().info { "Reconciling VirtualMachine: ${vm.spec.name}" }
 
-            if (vm.status == null) {
-                val vmModelResult = runBlocking { vmClient.getVMByName(vm.spec.name, vm.spec.getVmExtraInfo().applyId) }
-                if (vmModelResult.isSuccess) {
-                    vm.status = vmModelResult.getOrThrow().toCrdStatus()
-                    return UpdateControl.patchStatus(vm).rescheduleAfter(10000L)
-                } else {
-                    val vmApply = VmApply.id(vm.spec.getVmExtraInfo().applyId)
-                    if (vmApply == null || !vmApply.isApproved()) {
-                        return UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
-                    }
-                    val vmModel = runBlocking {
-                        if (createVmProcessMutex.tryLock(vm)) {
-                            try {
-                                vmClient.createVM(vm.spec.toCreateVmOptions()).getOrThrow()
-                            } finally {
-                                createVmProcessMutex.unlock(vm)
+                if (vm.status == null) {
+                    val vmModelResult = runBlocking { vmClient.getVMByName(vm.spec.name, vm.spec.getVmExtraInfo().applyId) }
+                    if (vmModelResult.isSuccess) {
+                        vm.status = vmModelResult.getOrThrow().toCrdStatus()
+                        return UpdateControl.patchStatus(vm).rescheduleAfter(10000L)
+                    } else {
+                        val vmApply = VmApply.id(vm.spec.getVmExtraInfo().applyId)
+                        if (vmApply == null || !vmApply.isApproved()) {
+                            return UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
+                        }
+                        val vmModel = runBlocking {
+                            if (createVmProcessMutex.tryLock(vm)) {
+                                try {
+                                    vmClient.createVM(vm.spec.toCreateVmOptions()).getOrThrow()
+                                } finally {
+                                    createVmProcessMutex.unlock(vm)
+                                }
+                            } else {
+                                null
                             }
+                        }
+                        return if (vmModel != null) {
+                            vm.status = vmModel.toCrdStatus()
+                            UpdateControl.patchStatus(vm).rescheduleAfter(10000L)
                         } else {
-                            null
+                            UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
                         }
                     }
-                    return if (vmModel != null) {
-                        vm.status = vmModel.toCrdStatus()
-                        UpdateControl.patchStatus(vm).rescheduleAfter(10000L)
+                }
+
+                assert(vm.status != null)
+
+                // check if all done
+                val vmApply = VmApply.id(vm.spec.getVmExtraInfo().applyId)
+                if (vmApply != null && !vmApply.done) {
+                    val done = vmKubeClient
+                        .inNamespace(vmApply.namespaceName())
+                        .list()
+                        .items
+                        .all {
+                            val extraInfo = it.spec.getVmExtraInfo()
+                            extraInfo.initial && it.status != null
+                        }
+                    if (done) {
+                        vmApply.done = true
+                        mysql.vmApplyList.update(vmApply)
+                    }
+                }
+
+                val vmUuid = vm.status.uuid
+
+                runBlocking {
+                    val vmModel = vmClient.getVM(vmUuid).getOrNull()
+                    if (vmModel == null) {
+                        vm.spec = vm.spec.copy(deleted = true)
                     } else {
-                        UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
+                        vm.status = vmModel.toCrdStatus()
                     }
                 }
-            }
 
-            assert(vm.status != null)
-
-            // check if all done
-            val vmApply = VmApply.id(vm.spec.getVmExtraInfo().applyId)
-            if (vmApply != null && !vmApply.done) {
-                val done = vmKubeClient
-                    .inNamespace(vmApply.namespaceName())
-                    .list()
-                    .items
-                    .all {
-                        val extraInfo = it.spec.getVmExtraInfo()
-                        extraInfo.initial && it.status != null
+                // check the power
+                if (vm.spec.powerState == VirtualMachineModel.PowerState.PoweredOn) {
+                    if (vm.status.powerState != VirtualMachineModel.PowerState.PoweredOn) {
+                        runBlocking {
+                            vmClient.powerOnSync(vmUuid)
+                        }
                     }
-                if (done) {
-                    vmApply.done = true
-                    mysql.vmApplyList.update(vmApply)
-                }
-            }
-
-            val vmUuid = vm.status.uuid
-
-            runBlocking {
-                val vmModel = vmClient.getVM(vmUuid).getOrNull()
-                if (vmModel == null) {
-                    vm.spec = vm.spec.copy(deleted = true)
-                } else {
-                    vm.status = vmModel.toCrdStatus()
-                }
-            }
-
-            // check the power
-            if (vm.spec.powerState == VirtualMachineModel.PowerState.PoweredOn) {
-                if (vm.status.powerState != VirtualMachineModel.PowerState.PoweredOn) {
-                    runBlocking {
-                        vmClient.powerOnSync(vmUuid)
+                } else if (vm.spec.powerState == VirtualMachineModel.PowerState.PoweredOff) {
+                    if (vm.status.powerState != VirtualMachineModel.PowerState.PoweredOff) {
+                        runBlocking {
+                            vmClient.powerOffSync(vmUuid)
+                        }
                     }
                 }
-            } else if (vm.spec.powerState == VirtualMachineModel.PowerState.PoweredOff) {
-                if (vm.status.powerState != VirtualMachineModel.PowerState.PoweredOff) {
-                    runBlocking {
-                        vmClient.powerOffSync(vmUuid)
-                    }
-                }
-            }
 
-            return UpdateControl.patchStatus(vm).rescheduleAfter(10000L)
-        } catch (e: Throwable) {
-            logger("vm-reconcile")().error { "Reconciling virtual machine error: ${e.localizedMessage}" }
-            return UpdateControl.noUpdate<VirtualMachine>().rescheduleAfter(10000L)
+                return UpdateControl.patchStatus(vm).rescheduleAfter(10000L)
+            } catch (e: Throwable) {
+                logger("vm-reconcile")().error { "Reconciling virtual machine error: ${e.localizedMessage}" }
+            }
+            Thread.sleep(5000L);
         }
+        return UpdateControl.noUpdate()
     }
 
     override fun cleanup(resource: VirtualMachine?, context: Context<VirtualMachine>?): DeleteControl {
