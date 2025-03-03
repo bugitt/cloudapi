04a2b265ec4a78df5684f281f9ad4f1797f14125
HanZiyao <49408466+h56983577@users.noreply.github.com>
2025-03-04 01:01:31 +0800

    资源池名称只能为小写和数字 (#82)
    * 资源池名称只能为小写和数字
    
    * 改为国外maven源
    
    * 去掉构建过程中的内存限制
    
    * 去掉创建k8s用户资源的步骤

--- cloudapi-web/build.gradle.kts
+++ cloudapi-web/build.gradle.kts
@@ -20,15 +20,8 @@ kotlin {
 }
 
 repositories {
-    maven { setUrl("https://maven.aliyun.com/repository/central") }
-    maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-    maven { setUrl("https://maven.aliyun.com/repository/google") }
-    maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-    maven { setUrl("https://maven.aliyun.com/repository/public") }
-    maven { setUrl("https://jitpack.io") }
-    gradlePluginPortal()
-    google()
     mavenCentral()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
